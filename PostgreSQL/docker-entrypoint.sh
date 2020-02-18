bf157493d563ac573ec9efde7943d86bf79ca6d2
Emaloka <guih.smi2@outlook.com>
2020-02-18 13:49:17 -0300

    Servidor Funcilnal com .sh

--- PostgreSQL/docker-entrypoint.sh
+++ PostgreSQL/docker-entrypoint.sh
@@ -5,4 +5,13 @@ echo '*==============================================*'
 echo '*=   Iniciando o Banco de Dados PostgreSQL    =*'
 echo '*==============================================*'
 echo $1
-postgres -D /etc/postgresql/data
+# Inicia o servidor de banco de dados
+pg_ctl -D /etc/postgresql/data/ start 
+# Inicia o Backup de Inicio de Desenvolvimento
+echo '*==============================================*'
+echo '*= Backup do arquivo TXT para Desenvolvimento =*'
+echo '*==============================================*'
+#cat /home/app/db/backup.txt & psql -U 
+#if [$? 
+# Deixa o processo em execução ate que seja cancelado
+sleep infinity & wait
