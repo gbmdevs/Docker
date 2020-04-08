60752a1c3568456e3531e9a9a23895e21d2767dc
Emaloka <guih.smi2@outlook.com>
2020-04-08 12:42:52 -0300

    Att React Entrypoint.sh

---
+++ React/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo '* Incializacao do React *'
+
+# Incluir mais Watcher para Requisição
+echo fs.inotify.max_user_watches=524288 | tee -a /etc/sysctl.conf
+sysctl -p
+
+cat /proc/sys/fs/inotify/max_user_watches
+
+sleep infinity
