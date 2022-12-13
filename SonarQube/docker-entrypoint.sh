556c850bd182653fbd52c0afbe4b748cb563823c
Guilherme Gois Braga <guilherme.gois@tqi.com.br>
2022-12-13 18:24:17 -0300

    Inicio do SonarQube

---
+++ SonarQube/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 
+# 
+set -e
+echo '*==============================================*'
+echo '*=   Iniciando SonarQuube - Qualidade Code    =*'
+echo '*==============================================*'
+/home/desenv/sonarqube/sonarqube-7.8/bin/linux-x86-64/sonar.sh start
+sleep infinity
+ 
+ 
